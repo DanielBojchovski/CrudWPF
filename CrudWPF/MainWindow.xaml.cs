f440cb690649fe25db140ccf8f81f53a9012d4c0
Your Name <you@example.com>
2022-10-26 10:50:28 +0200

    Add project files.

---
+++ CrudWPF/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CrudWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadGrid();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=NewDB;Integrated Security=True");
+
+        public void clearData()
+        {
+            name_txt.Clear();
+            age_txt.Clear();
+            gender_txt.Clear();
+            city_txt.Clear();
+            search_txt.Clear();
+        }
+
+        public void LoadGrid()
+        {
+            SqlCommand cmd = new SqlCommand("select * from FirstTable", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            datagrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            clearData(); 
+        }
+
+        public bool isValid()
+        {
+            if(name_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Enter name", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (age_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Enter age", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (gender_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Enter gender", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (city_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Enter city", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void InsertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (isValid())
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO FirstTable VALUES (@Name, @Age, @Gender, @City)", con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@Name", name_txt.Text);
+                    cmd.Parameters.AddWithValue("@Age", age_txt.Text);
+                    cmd.Parameters.AddWithValue("@Gender", gender_txt.Text);
+                    cmd.Parameters.AddWithValue("@City", city_txt.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    LoadGrid();
+                    MessageBox.Show("Record successfuly added", "Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                    clearData();
+                }
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show($"Error occured - {ex.Message}");
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand($"delete from FirstTable where ID = '{search_txt.Text}'", con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record successfuly deleted", "Deleted", MessageBoxButton.OK, MessageBoxImage.Information);
+                con.Close();
+                clearData();
+                LoadGrid();
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Error occured - {ex.Message}");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand($"update FirstTable set Name = '{name_txt.Text}', Age = '{age_txt.Text}', Gender = '{gender_txt.Text}', City = '{city_txt.Text}' where ID = '{search_txt.Text}'", con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record successfuly updated", "Updated", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show($"Error occured - {ex.Message}");
+            }
+            finally
+            {
+                con.Close();
+                clearData();
+                LoadGrid();
+            }
+        }
+    }
+}
